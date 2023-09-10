22beff308797be725add7955e5ba2bca17f84d38
Lwandile Mfundisi <Lwandile.Mfundisi@media24.com>
2023-09-10 21:57:02 +0200

    Add project files.

---
+++ XFrame.Aggregates.Queries.Extensions/DomainContainerQueryExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using XFrame.DomainContainers;
+
+namespace XFrame.Aggregates.Queries.Extensions
+{
+    public static class DomainContainerQueryExtensions
+    {
+        public static IDomainContainer AddQueries(
+            this IDomainContainer domainContainer,
+            params Type[] queryTypes)
+        {
+            return domainContainer.AddQueries(queryTypes);
+        }
+
+        public static IDomainContainer AddQueries(
+            this IDomainContainer domainContainer,
+            Assembly fromAssembly,
+            Predicate<Type> predicate)
+        {
+            predicate = predicate ?? (t => true);
+            var queryTypes = fromAssembly
+                .GetTypes()
+                .Where(t => !t.GetTypeInfo().IsAbstract && typeof(IQuery).GetTypeInfo().IsAssignableFrom(t))
+                .Where(t => predicate(t));
+            return domainContainer.AddTypes(queryTypes);
+        }
+    }
+}
